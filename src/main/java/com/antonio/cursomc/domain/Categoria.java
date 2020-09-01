516b381d5f390376621710e2d404c4cfa7236903
Antonio Carlos da silva Moreira <antoniomoreira@fgfconsultores.com.br>
2020-09-01 18:20:53 -0300

    Aula 16 - Banco de dados PG e criação automática da base de dados

--- src/main/java/com/antonio/cursomc/domain/Categoria.java
+++ src/main/java/com/antonio/cursomc/domain/Categoria.java
@@ -2,12 +2,18 @@ package com.antonio.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,7 +22,10 @@ import lombok.Setter;
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
-	
+
 }
