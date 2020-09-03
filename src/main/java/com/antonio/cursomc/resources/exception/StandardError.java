a12d552d19b8ab52965c0b36ca9be2b83dab0758
Antonio Carlos da silva Moreira <antoniomoreira@FGFCONSULTORES.com.br>
2020-09-03 09:41:47 -0300

    Aula 21 - Estado e Cidade - tabelas

--- src/main/java/com/antonio/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/antonio/cursomc/resources/exception/StandardError.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
--- src/main/java/com/antonio/cursomc/services/CategoriaService.java
+++ src/main/java/com/antonio/cursomc/services/CategoriaService.java
@@ -11,15 +11,14 @@ import com.antonio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	//buscar uma categoria por código
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
 				+ ", Tipo: " + Categoria.class.getName()));
 	}
-
 }
